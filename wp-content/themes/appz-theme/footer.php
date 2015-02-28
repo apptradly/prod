552c18aeb853b84bc92bb868122908a9bd9a7c5b
apptradly <info@tradly.co>
2015-02-28 23:27:06 +0530

    Version 1
    tradly website

---
+++ wp-content/themes/appz-theme/footer.php
@@ -0,0 +1,11 @@
+<div class="container_12">
+	<footer id="footer" class="grid_12">
+		<p class="left"><?php echo theme_option('theme_copyright', __('&copy; 2013 Appz. All Rights Reserved.', TDOMAIN)); ?></p>
+
+		<p class="right"><a href="">Made in India</a>.</p>
+	</footer>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
